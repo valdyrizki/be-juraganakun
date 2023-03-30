4b6b40ee556a0e8b8ce0985433329cbba0ec3cb6
Valdy Rizki Djuliansyah <51867202+valdyrizki@users.noreply.github.com>
2023-03-30 14:10:30 +0700

    TWO-202303-03
    Tambahkan preview file di menu transaksi saat user add product + bug fixing search data by invoice

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -7,82 +7,6 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function get()
     {
         $isSuccess = true;
@@ -114,6 +38,19 @@ class FileController extends Controller
         $isSuccess = true;
         $msg = 'SUCCESS';
         $data = ProductFile::where('product_id',$request->product_id)->get();
+
+        return response()->json([
+            'isSuccess' => $isSuccess,
+            'msg' => $msg,
+            'data' => $data,
+        ]);
+    }
+
+    public function getPreviewFile(Request $request)
+    {
+        $isSuccess = true;
+        $msg = 'SUCCESS';
+        $data = ProductFile::whereNull('invoice_id')->where("product_id",$request->product_id)->take($request->qty)->get();
 
         return response()->json([
             'isSuccess' => $isSuccess,
